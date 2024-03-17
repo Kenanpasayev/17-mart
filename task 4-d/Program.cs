eab17afada78d1bd52a423b9f1c88ebab4ed8d80
Kenanpasayev <tu7kr6n7r@code.edu.az>
2024-03-17 17:08:07 +0400

    method task

---
+++ task 4-d/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace task_4_d
+{
+    internal class Program
+    {
+        //Ucbucaqlinin daxiline cekilmis cevrenin sahesi-S=p*r:p=(a+b+c)/2
+        public static int area ( int a, int b, int c, int r) 
+        {
+            int p =(a+b+c)/2;
+            return p * r;
+        }
+        static void Main(string[] args)
+        {
+            int a =3;
+            int b =4;
+            int c =5;
+            int r = 6;
+            Console.WriteLine(area(a,b,c,r));
+        }
+    }
+}
